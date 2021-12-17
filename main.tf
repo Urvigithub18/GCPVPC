a027c03ce8a11e011eb4248c13d5bf967241e22d
Urvigithub18 <95743865+Urvigithub18@users.noreply.github.com>
2021-12-17 17:33:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,53 @@
-resource "google_compute_network" "default" {
-    name                    = "{var.name}"
-    project                 = "{var.projectname}"
-    auto_create_subnetworks = "{var.automatic_create_subnetworks}"
-    routing_mode            = "{var.routing_mode}"
+module "vpc" {
+  source                                 = "./modules/vpc"
+  network_name                           = var.network_name
+  auto_create_subnetworks                = var.auto_create_subnetworks
+  routing_mode                           = var.routing_mode
+  project_id                             = var.project_id
+  description                            = var.description
+  shared_vpc_host                        = var.shared_vpc_host
+  delete_default_internet_gateway_routes = var.delete_default_internet_gateway_routes
+  mtu                                    = var.mtu
+}
+    
+module "subnets" {
+  source           = "./modules/subnets"
+  project_id       = var.project_id
+  network_name     = module.vpc.network_name
+  subnets          = var.subnets
+  secondary_ranges = var.secondary_ranges
 }
 
-  resource "google_compute_subnetwork" "default" {
-      count         = "{var.automatic_create_subnetworks == "false" ? length(var.subnetworks) : 0}"
-      project       = "{var.projectname}"
-      name          = "{var.subnetworks[count.index].name}-${var.subnetworks[count.index].region}" 
-      ip_cidr_range = "{var.subnetworks[count.index].cidr}"
-      region        = "{var.subnetworks[count.index].region}"
-      network       = "{google_compute_network.default.self_link}" 
+module "routes" {
+  source            = "./modules/routes"
+  project_id        = var.project_id
+  network_name      = module.vpc.network_name
+  routes            = var.routes
+  module_depends_on = [module.subnets.subnets]
+}
 
-      private_ip_google_access  = "${var.private_ip_google_access}"
-      enable_flow_logs          = "${var.enable_flow_logs}"
+locals {
+  rules = [
+    for f in var.firewall_rules : {
+      name                    = f.name
+      direction               = f.direction
+      priority                = lookup(f, "priority", null)
+      description             = lookup(f, "description", null)
+      ranges                  = lookup(f, "ranges", null)
+      source_tags             = lookup(f, "source_tags", null)
+      source_service_accounts = lookup(f, "source_service_accounts", null)
+      target_tags             = lookup(f, "target_tags", null)
+      target_service_accounts = lookup(f, "target_service_accounts", null)
+      allow                   = lookup(f, "allow", [])
+      deny                    = lookup(f, "deny", [])
+      log_config              = lookup(f, "log_config", null)
+    }
+  ]
+}
 
-      dynamic "secondary_ip_range" {
-          iterator = ip
-          for_each = var.subnetworks[count.index].secondary_ip_range
-
-          content {
-              range_name    = ip.value.name
-              ip_cidr_range = ip.value.cidr
-          }
-      }
-  }
+module "firewall_rules" {
+  source       = "./modules/firewall-rules"
+  project_id   = var.project_id
+  network_name = module.vpc.network_name
+  rules        = local.rules
+}
